9b0e167911cd8cf3ba82c8a54facfd964510dccd
Joel-java-jpg <Joel.schou@iths.se>
2025-09-17 13:27:01 +0200

    Initial commit

---
+++ src/main/java/se/iths/joel/project_1/Project1.java
@@ -0,0 +1,16 @@
+package se.iths.joel.project_1;
+
+public class Project1 {
+    public static void main(String[] args) {
+
+        String[] ord = {"Hello", "World", "!"};
+
+        skrivUtArray(ord);
+    }
+
+    public static void skrivUtArray(String[] ord) {
+        for (String element : ord) {
+            System.out.println(element);
+        }
+    }
+}
